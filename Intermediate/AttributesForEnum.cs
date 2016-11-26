3b4cf30e82fd9289a3f7b497cf18b08391c36a73
Robert Dresler <Robert.Dresler@fnz.com>
2016-11-26 07:31:16 +0100

    Reworked to run from tests (xunit)

---
+++ Intermediate/AttributesForEnum.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace CSharpDrill.Intermediate
+{
+    [Tags("Enum", "Attribute", "Extension method")]
+    public class AttributesForEnum
+    {
+        // - Uncomment content of the test method.
+        // - Create RequiredSkill attribute class and use it for decorating DeveloperType values (see the example for BackEndDeveloper).
+        // - Create an extension method for a DeveloperType value that returns required skills as IReadOnlyCollection<DeveloperSkillType>.
+        // - All tests must pass.
+
+        [Theory]
+        [InlineData(DeveloperType.BackEndDeveloper, new [] { DeveloperSkillType.CSharp, DeveloperSkillType.VisualBasic, DeveloperSkillType.MsSql })]
+        [InlineData(DeveloperType.FrontEndDeveloper, new [] { DeveloperSkillType.CSharp, DeveloperSkillType.JavaScript, DeveloperSkillType.Mvc, DeveloperSkillType.React })]
+        [InlineData(DeveloperType.ServiceDeveloper, new [] { DeveloperSkillType.CSharp, DeveloperSkillType.MsSql, DeveloperSkillType.SoapRest })]
+        [InlineData(DeveloperType.FakeDeveloper, new DeveloperSkillType[0])]
+        public void GetRequiredSkills_ForDeveloperType_ShouldReturnExpected(DeveloperType developerType, DeveloperSkillType[] expectedDeveloperSkills)
+        {
+            //var result = developerType.GetRequiredSkills();
+            //result.ShouldBeEquivalentTo(expectedDeveloperSkills);
+        }
+    }
+
+    public enum DeveloperSkillType
+    {
+        CSharp,
+        VisualBasic,
+        MsSql,
+        SoapRest,
+        Mvc,
+        React,
+        JavaScript
+    }
+
+    public enum DeveloperType
+    {
+        //[RequiredSkill(DeveloperSkillType.CSharp)]
+        BackEndDeveloper,
+
+        FrontEndDeveloper,
+
+        ServiceDeveloper,
+
+        FakeDeveloper
+    }
+}
---
+++ TagsAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpDrill
+{
+    public sealed class TagsAttribute : Attribute
+    {
+        public string[] Tags { get; }
+
+        public TagsAttribute(params string[] tags)
+        {
+            Tags = tags;
+        }
+    }
+}
