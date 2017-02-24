ceb6e2d7c4f879de71e7715dc6b24ae1a158423f
Robert Dresler <dresler.robert@gmail.com>
2017-02-24 20:46:29 +0100

    DeadlockMaker added

---
+++ Advanced/DeadlockMaker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CSharpDrill.Advanced
+{
+    public class DeadlockMakerTests
+    {
+        // Task: Implement a deadlock in DeadlockMaker.Do()
+
+        [Fact]
+        public void Do_ShouldTimedOut()
+        {
+            var timedOut = true;
+
+            var task = Task.Run(() =>
+            {
+                DeadlockMaker.Do();
+                timedOut = false;
+            });
+
+            task.Wait(TimeSpan.FromSeconds(1));
+
+            Assert.True(timedOut);
+        }
+    }
+
+    public class DeadlockMaker
+    {
+        public static void Do()
+        {
+            var taskA = Task.Run(() => { });
+
+            var taskB = Task.Run(() => { });
+
+            Task.WaitAll(taskA, taskB);
+        }
+    }
+}
