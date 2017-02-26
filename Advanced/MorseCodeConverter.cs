2a60740a4f1d7b1ae45c6367e0533de4c4e96176
Robert Dresler <dresler.robert@gmail.com>
2017-02-26 10:20:51 +0100

    MorseCodeConverter added

---
+++ Advanced/MorseCodeConverter.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Xunit;
+
+namespace CSharpDrill.Advanced
+{
+    public class MorseCodeConverterTests
+    {
+        // Task: Implement MorseCodeConverter.ToMorseCode
+        // Don't care about invalid characters.
+
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("a", ".-")]
+        [InlineData("A", ".-")]
+        [InlineData("42", "....-|..---")]
+        [InlineData("Keep@it@secret!", "-.-|.|.|.--.|.--.-.|..|-|.--.-.|...|.|-.-.|.-.|.|-|-.-.--")]
+        public void ToMorseCode_ForGivenText_ShouldReturnExpected(string text, string expectedMorseCode)
+        {
+            var morseCode = MorseCodeConverter.ToMorseCode(text);
+            Assert.Equal(expectedMorseCode, morseCode);
+        }
+
+        // Task: Implement MorseCodeConverter.FromMorseCode
+        // Don't care about invalid characters.
+
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("....|.|.-..|.--.|--|.", "helpme")]
+        [InlineData("...|.|-.-.|.-.|.|-|...--|..---|----.|----.|---..|--...", "secret329987")]
+        public void FromMorseCode_ForGivenText_ShouldReturnExpected(string morseCode, string expectedText)
+        {
+            var text = MorseCodeConverter.FromMorseCode(morseCode);
+            Assert.Equal(expectedText, text);
+        }
+    }
+
+    public static class MorseCodeConverter
+    {
+        public static string ToMorseCode(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string FromMorseCode(string morseCode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MorseCodeConstants
+    {
+        public static string MorseCodesSeparator = "|";
+        public static string MorseAlphabetAsJson =
+            @"
+{
+  ""0"": ""-----"",
+  ""1"": "".----"",
+  ""2"": ""..---"",
+  ""3"": ""...--"",
+  ""4"": ""....-"",
+  ""5"": ""....."",
+  ""6"": ""-...."",
+  ""7"": ""--..."",
+  ""8"": ""---.."",
+  ""9"": ""----."",
+  ""a"": "".-"",
+  ""b"": ""-..."",
+  ""c"": ""-.-."",
+  ""d"": ""-.."",
+  ""e"": ""."",
+  ""f"": ""..-."",
+  ""g"": ""--."",
+  ""h"": ""...."",
+  ""i"": "".."",
+  ""j"": "".---"",
+  ""k"": ""-.-"",
+  ""l"": "".-.."",
+  ""m"": ""--"",
+  ""n"": ""-."",
+  ""o"": ""---"",
+  ""p"": "".--."",
+  ""q"": ""--.-"",
+  ""r"": "".-."",
+  ""s"": ""..."",
+  ""t"": ""-"",
+  ""u"": ""..-"",
+  ""v"": ""...-"",
+  ""w"": "".--"",
+  ""x"": ""-..-"",
+  ""y"": ""-.--"",
+  ""z"": ""--.."",
+  ""."": "".-.-.-"",
+  "","": ""--..--"",
+  ""?"": ""..--.."",
+  ""!"": ""-.-.--"",
+  ""-"": ""-....-"",
+  ""/"": ""-..-."",
+  ""@"": "".--.-."",
+  ""("": ""-.--."",
+  "")"": ""-.--.-""
+}";
+    }
+}
+
